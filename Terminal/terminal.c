33831206de9b8d2941a3ecbf01cbace23904be02
HosamAboabla <hosamaboabla@yahoo.com>
2022-07-18 20:28:36 +0200

    Adding terminal module

---
+++ Terminal/terminal.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "terminal.h"
+
+
+EN_terminalError_t getTransactionDate(ST_terminalData_t *termData)
+{
+    // Format DD/MM/YYYY
+    uint8_t date[11];
+
+    printf("Please enter transaction date: ");
+    _flushall();
+    scanf("%s" , date);
+
+    if(date == NULL || strlen(date) != 10 || date[2] != '/' || date[5] != '/')
+        return WRONG_DATE;
+
+    strcpy(termData->transactionDate , date);
+
+    return OK;
+    
+}
+EN_terminalError_t isCardExpired(ST_cardData_t cardData, ST_terminalData_t termData)
+{
+    // Format "MM/YY"
+    int expiry_date_year , expiry_date_month , transation_date_year , transaction_date_month;
+
+    expiry_date_year = (cardData.cardExpirationDate[3] - '0' ) * 10 + (cardData.cardExpirationDate[4] - '0' ) + 2000;
+    expiry_date_month = ( cardData.cardExpirationDate[0] - '0') * 10 + ( cardData.cardExpirationDate[1] - '0');
+
+    transation_date_year = (termData.transactionDate[6] - '0' ) * 1000 
+                         + (termData.transactionDate[7] - '0' ) * 100
+                         + (termData.transactionDate[8] - '0' ) * 10
+                         + (termData.transactionDate[9] - '0' );
+
+    transaction_date_month = (termData.transactionDate[3] - '0') * 10 + (termData.transactionDate[4] - '0');
+
+
+    printf("Expiry year: %d\n" , expiry_date_year);
+    printf("Expiry month: %d\n" , expiry_date_month);
+
+    printf("Transation year: %d\n" , transation_date_year);
+    printf("Transation month: %d\n" , transaction_date_month);
+
+    if( expiry_date_year < transation_date_year )
+        return EXPIRED_CARD;
+    else if( expiry_date_year == transation_date_year && expiry_date_month < transaction_date_month)
+        return EXPIRED_CARD;
+
+    return TERMINAL_OK;
+}
+EN_terminalError_t getTransactionAmount(ST_terminalData_t *termData)
+{
+    float amount;
+    printf("Please enter transation amount: ");
+    _flushall();
+    scanf("%f" , &amount);
+
+    if(amount <= 0)
+        return INVALID_AMOUNT;
+    
+    termData->transAmount = amount;
+
+    return TERMINAL_OK;
+}
+EN_terminalError_t isBelowMaxAmount(ST_terminalData_t *termData)
+{
+    if( termData->transAmount > termData->maxTransAmount)
+        return EXCEED_MAX_AMOUNT;
+    
+    return TERMINAL_OK;
+}
+EN_terminalError_t setMaxAmount(ST_terminalData_t *termData)
+{
+    float max_amount;
+    printf("Please enter maximum transation amount: ");
+    _flushall();
+
+    scanf("%f" , &max_amount);
+
+    if( max_amount <= 0)
+        return INVALID_MAX_AMOUNT;
+    
+    termData->maxTransAmount = max_amount;
+    return TERMINAL_OK;
+}
+EN_terminalError_t isValidCardPAN(ST_cardData_t *cardData);
--- main.c
+++ main.c
@@ -3,26 +3,30 @@
 #include <string.h>
 
 #include "Card/card.h"
-
+#include "Terminal/terminal.h"
 
 int main()
 {
 
-    ST_cardData_t card;
-    getCardHolderName(&card);
-    getCardExpiryDate(&card);
-    getCardPAN(&card);
+    // ST_cardData_t card;
+    // getCardHolderName(&card);
+    // getCardExpiryDate(&card);
+    // getCardPAN(&card);
+    // printf("card holder name is: %s\n" , card.cardHolderName);
+    // printf("card expiry date is: %s\n" , card.cardExpirationDate);
+    // printf("card PAN is: %s\n" , card.primaryAccountNumber);
 
-    printf("card holder name is: %s\n" , card.cardHolderName);
-    printf("card expiry date is: %s\n" , card.cardExpirationDate);
-    printf("card PAN is: %s\n" , card.primaryAccountNumber);
+    ST_terminalData_t terminal;
 
-    // unsigned char test[100];
-    // unsigned char test2[100];
-    // printf("Please enter your name: ");
-    // scanf("%s" , test);
-    // strcpy(test2 , test);
-    // printf("hello %s\n" , test2);
+    // getTransactionDate(&terminal);
+    // printf("Transation date: %s\n" , terminal.transactionDate);
+    // if(isCardExpired( card , terminal)== TERMINAL_OK)
+    //     printf("Card is not expired\n");
+    // else
+    //     printf("Card is expired\n");
 
+    // printf("%d\n" ,getTransactionAmount(&terminal));
+    // printf("Transation amount = %f\n" , terminal.transAmount);
+    
     return 0;
 }
