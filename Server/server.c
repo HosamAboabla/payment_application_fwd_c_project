e25a521d2c7eee2a023c660edfcfc4d5ca437373
HosamAboabla <hosamaboabla@yahoo.com>
2022-07-22 19:53:35 +0200

    add db files

--- Server/server.c
+++ Server/server.c
@@ -18,12 +18,19 @@ uint32_t SEQUENCE_TRANSACTION_NUMBER = 0;
 EN_serverError_t isValidAccount(ST_cardData_t *cardData)
 {
     // Check if the account exists using primary account number
-    uint8_t accounts_len = accounts_len = sizeof(accounts) / sizeof(accounts[0]);
-    for(int i = 0 ; i < accounts_len ; i++)
+    float account_balance;
+    uint8_t account_pan[20];
+    FILE* accounts_file;
+
+    accounts_file = fopen("./DB/accounts.txt" , "r");
+
+    while( fscanf( accounts_file , "%f %s" , &account_balance , account_pan ) != EOF)
     {
-        if(strcmp(cardData->primaryAccountNumber , accounts[i].primaryAccountNumber) == 0)
+        if(strcmp( cardData->primaryAccountNumber , account_pan) == 0)
             return SERVER_OK;
     }
+    fclose(accounts_file);
+
     return DECLINED_STOLEN_CARD;
 }
 
@@ -39,24 +46,45 @@ uint8_t getAccountIndex(ST_cardData_t *cardData)
 
 EN_serverError_t isAmountAvailable(ST_terminalData_t *termData , ST_cardData_t *cardData)
 {
-    uint8_t account_index;
-    
-    account_index = getAccountIndex(cardData);
 
-    if(termData->transAmount > accounts[account_index].balance)
-        return LOW_BALANCE;
-    
+    float account_balance;
+    uint8_t account_pan[20];
+    FILE* accounts_file;
+
+    accounts_file = fopen("./DB/accounts.txt" , "r");
+
+    while( fscanf( accounts_file , "%f %s" , &account_balance , account_pan ) != EOF)
+    {
+        if(strcmp( cardData->primaryAccountNumber , account_pan) == 0)
+        {
+            if(termData->transAmount > account_balance)
+                return LOW_BALANCE;
+        }
+    }
+    fclose(accounts_file);
+
     return SERVER_OK;
 }
 
 EN_serverError_t saveTransaction(ST_transaction_t *transData)
 {
-    if(SEQUENCE_TRANSACTION_NUMBER >= 255 )
+
+    FILE* transactions_file;
+    uint32_t seqNumber = 1;
+    transactions_file = fopen("./DB/transactions.txt" , "a+");
+
+    if(transactions_file == NULL)
         return SAVING_FAILED;
 
-    transData->transactionSequenceNumber = SEQUENCE_TRANSACTION_NUMBER;
-    transactions[SEQUENCE_TRANSACTION_NUMBER] = *transData;
-    SEQUENCE_TRANSACTION_NUMBER++;
+    while( fscanf(transactions_file , "%*[^\n]\n" ) != EOF)
+        seqNumber++;
+
+    transData->transactionSequenceNumber = seqNumber;
+    fprintf(transactions_file , "%s %s %s %f %f %s %d %ld\n" , 
+     transData->cardHolderData.cardHolderName , transData->cardHolderData.primaryAccountNumber, transData->cardHolderData.cardExpirationDate,
+     transData->terminalData.maxTransAmount , transData->terminalData.transAmount , transData->terminalData.transactionDate,
+     transData->transState , transData->transactionSequenceNumber);
+    fclose(transactions_file);
 
     return SERVER_OK;
 }
@@ -66,33 +94,29 @@ EN_transState_t recieveTransactionData(ST_transaction_t *transData)
     uint8_t accounts_len , account_index;
     accounts_len = sizeof(accounts) / sizeof(accounts[0]);
     
+    transData->transState = APPROVED;
     // Check if the account exists using primary account number
     if( isValidAccount( &transData->cardHolderData) == DECLINED_STOLEN_CARD)
     {
         transData->transState = DECLINED_STOLEN_CARD;
-        return DECLINED_STOLEN_CARD;
     }
 
     // check if the amount is available or not
     if(isAmountAvailable(&transData->terminalData , &transData->cardHolderData) == LOW_BALANCE)
     {
         transData->transState = DECLINED_INSUFFECIENT_FUND;
-        return DECLINED_INSUFFECIENT_FUND;
     }
     
-    account_index = getAccountIndex(&transData->cardHolderData);
-
     // save transation data
     if( saveTransaction(transData) == SAVING_FAILED )
     {
-        transData->transState = INTERNAL_SERVER_ERROR;
-        return SAVING_FAILED;
+        return INTERNAL_SERVER_ERROR;
     }
     
     accounts[account_index].balance -= transData->terminalData.transAmount;
     transData->transState = APPROVED;
 
-    return APPROVED;
+    return transData->transState;
 }
 EN_serverError_t getTransaction(uint32_t transactionSequenceNumber, ST_transaction_t *transData)
 {
